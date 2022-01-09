dea50f45333b0b0e7bcacc8e7a4ae67616c9f66e
HongDuHyeon <ghdengus12@naver.com>
2022-01-09 23:53:47 +0900

    [git] todo 삭제

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event) {
+    const deleteEl = event.target.parentElement; 
+    // event는 paintToDo에 있는 newBtn에 대한 element info를 가져옴
+    // target은 click된 html element
+    // parentElement 는 click된 element의 부모 요소
+
+    deleteEl.remove();
+}
+
+
 function paintToDo(newTodo) {
     // console.log("i will paint " + newTodo); // 3. input에 들어온 value를 뿌려주는 역할
     const newLi = document.createElement("li");
     const newSpan = document.createElement("span");
-    newLi.appendChild(newSpan);
     newSpan.innerText = newTodo;
-    toDoList.appendChild(newLi)
+    const newBtn = document.createElement("button")
+    newBtn.innerText = '❌';
+    newBtn.addEventListener("click", deleteTodo)
+    newLi.appendChild(newSpan);
+    newLi.appendChild(newBtn);
+    toDoList.appendChild(newLi);
 }
 
 function handleToDoSubmit (e) {
