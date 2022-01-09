d156f4787bd4c94369eccd2b5457b66e89e63a18
HongDuHyeon <ghdengus12@naver.com>
2022-01-09 21:56:44 +0900

    [git] bg 이미지용 랜덤 이미지 불러오기

---
+++ js/background.js
@@ -0,0 +1,23 @@
+const images = [
+    "bg_01.jpg",
+    "bg_02.jpg",
+    "bg_03.jpg",
+    "bg_04.jpg",
+    "bg_05.jpg",
+    "bg_06.jpg",
+    "bg_07.jpg",
+    "bg_08.jpg",
+    "bg_09.jpg",
+]
+
+// images 배열에서 랜덤으로 가져오기
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// js에서 만든 태그를 html에 추가하기
+const bgImage = document.createElement("img");
+
+bgImage.src = `images/${chosenImage}`;
+
+console.log(bgImage);
+
+document.body.appendChild(bgImage)
